695ae5ae506e626425abc0fb0673c0cb504b1085
flatdesign <35264427+flatdesign@users.noreply.github.com>
2019-02-01 16:59:31 +0300

    Базовая структура

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-success" @click.prevent="loadData">Загрузить данные</button>
+    <my-table :data="getTableContent"></my-table>
+  </div>
+</template>
+
+<script>
+  import table from './components/Table.vue';
+  import axios from 'axios';
+
+  export default {
+    name: 'app',
+    components: {
+      'my-table': table
+    },
+    data () {
+      return {
+        msg: 'Hello Danil',
+        urlAPI: 'https://jsonplaceholder.typicode.com/albums',
+        tableContent: []
+      }
+    },
+    computed: {
+      getTableContent() {
+        return this.tableContent;
+      }
+    },
+    methods: {
+      async makeRequest(url) {
+        try {
+          let responce = await axios.get(url);
+          return responce;
+        } catch (e) {
+          console.log(e);
+          alert('Не удалось загрузить данные с сервера');
+        }
+      },
+      loadData() {
+        this.makeRequest(this.urlAPI).then(responce => this.tableContent = responce.data);
+        console.log(this.tableContent);
+      }
+    }
+  }
+</script>
+
+<style>
+ .container {
+    padding-top: 50px;
+ }
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="wrapper">
+    <table>
+      <tr>
+        <th v-for="(value, key) in getFilters" :key="key" @click="sort">
+          {{key}}<span :class=" getFilters[key] == 'down' ? 'fa fa-sort-desc' : 'fa-sort-asc'"></span>
+        </th>
+      </tr>
+      <tr v-for="(row, index) in data" :key="index">
+        <td v-for="(elem, index) in row" :key="index">
+          {{elem}}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['data'],
+    data() {
+      return {
+        filters: []
+      }
+    },
+    computed: {
+      getFilters() {
+        let obj = {};
+        this.filters = [];
+
+        for (let key in this.data[0]) {
+          obj[key] = 'down'
+        }
+        this.filters.push(obj);
+        return this.filters[0];
+      }
+    },
+    methods: {
+      sort() {
+        console.log('sort');
+      }
+    }
+  }
+</script>
+
+<style>
+  th:hover {
+    cursor: pointer;
+  }
+  table {
+    width: 80%;
+    border-collapse: collapse;
+    margin-top: 20px;
+    margin-bottom: 30px;
+  }
+  th, td {
+    border: 1px solid lightgray;
+    padding: 8px 15px;
+  }
+
+</style>
+
+
